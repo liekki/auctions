3250da179546df5b33c43156d566aa9fefb1c334
Matti Vakkilainen <matti.vakkilainen@yle.fi>
2017-02-01 20:01:22 +0200

    fix icons pt. 2

--- src/client/components/Item/Item.jsx
+++ src/client/components/Item/Item.jsx
@@ -5,7 +5,7 @@ export default ({data}) => {
   return (
     <div className="item">
       <Link to={`/item/${data.id}/${data.rand}`}>
-        <span className='icon' style={{ backgroundImage: `url(/static/img/icons/large/${data.item.icon}.png)`}} />
+        <span className='icon' style={{ backgroundImage: `url(/static/img/icons/large/${data.item.icon.toLowerCase()}.png)`}} />
         <span className={`name q${data.item.quality}`}>{data.fullName}</span>
         {data.suffix && <span className="modifiers">{data.suffix.modifiers}</span>}
       </Link>
