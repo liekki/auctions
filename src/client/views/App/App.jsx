4d671f3fae97eb1aa0b087dcefe01f306ab1d344
Matti Vakkilainen <matti.vakkilainen@yle.fi>
2017-02-02 10:36:55 +0200

    add a notice about recruitment

--- src/client/views/App/App.jsx
+++ src/client/views/App/App.jsx
@@ -11,7 +11,7 @@ export default ({main, header}) => {
       </div>
       <footer className="footer">
         <div className="footer-content">
-          Built with <span title="rage!" className="rage"></span> by Variola from &lt;Theorycraft&gt;
+          Built with <span title="rage!" className="rage"></span> by Variola from &lt;<a target="_blank" href="http://forum.theorycraft.fi/topic/185/we-are-currently-looking-for-the-following-classes" className="theorycraft">Theorycraft</a>&gt;
         </div>
       </footer>
     </div>
